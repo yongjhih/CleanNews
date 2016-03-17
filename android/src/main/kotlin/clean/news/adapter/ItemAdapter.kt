08d142596a258e32cac98f4160aaf6e53249d50c
Michael Pardo <kensuke155@gmail.com>
2016-03-17 10:28:55 -0400

    Formatting. Dependencies. Android template.

---
+++ android/src/main/kotlin/clean/news/adapter/ItemAdapter.kt
@@ -0,0 +1,70 @@
+package clean.news.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import clean.news.adapter.ItemAdapter.AbsItem
+import clean.news.adapter.ItemAdapter.AbsViewHolder
+import clean.news.entity.Item
+import clean.news.entity.Item.Type
+
+class ItemAdapter : RecyclerView.Adapter<AbsViewHolder<AbsItem>>() {
+	private val items = mutableListOf<AbsItem>()
+
+	fun setItems(items: List<Item>) {
+		this.items.clear()
+		this.items.addAll(items.map {
+			when (it.type) {
+				Type.STORY -> StoryItem(it)
+				else -> StoryItem(it)
+			}
+		})
+		notifyDataSetChanged()
+	}
+
+	override fun onBindViewHolder(viewHolder: AbsViewHolder<AbsItem>, position: Int) {
+		viewHolder.bind(position, items[position])
+	}
+
+	override fun onCreateViewHolder(viewHolder: ViewGroup, position: Int): AbsViewHolder<AbsItem> {
+		// TODO: inflate view holder
+		throw UnsupportedOperationException()
+	}
+
+	override fun getItemCount(): Int {
+		return items.size
+	}
+
+	override fun getItemViewType(position: Int): Int {
+		return items[position].getType()
+	}
+
+	// Adapter items
+
+	private class StoryItem(item: Item) : AbsItem(item) {
+		override fun getType(): Int {
+			return TYPE_STORY_ITEM
+		}
+	}
+
+	// View holders
+
+	private class StoryViewHolder(view: View) : AbsViewHolder<AbsItem>(view) {
+
+	}
+
+	// Abstract inner classes
+
+	abstract class AbsItem(val item: Item) {
+		abstract fun getType(): Int
+	}
+
+	abstract class AbsViewHolder<T : AbsItem>(view: View) : RecyclerView.ViewHolder(view) {
+		open fun bind(position: Int, item: T) {
+		}
+	}
+
+	companion object {
+		private const val TYPE_STORY_ITEM = 0
+	}
+}
---
+++ android/src/main/kotlin/clean/news/ui/view/ItemListView.kt
@@ -0,0 +1,37 @@
+package clean.news.ui.view
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.util.AttributeSet
+import clean.news.adapter.ItemAdapter
+import clean.news.model.ItemListViewModel
+import rx.android.schedulers.AndroidSchedulers
+import rx.subscriptions.CompositeSubscription
+import javax.inject.Inject
+
+class ItemListView : RecyclerView {
+	@Inject
+	lateinit var model: ItemListViewModel
+
+	private val adapter = ItemAdapter()
+	private val subscriptions = CompositeSubscription()
+
+
+	@JvmOverloads
+	constructor(context: Context, attrs: AttributeSet? = null, defStyle: Int = 0) : super(context, attrs, defStyle) {
+	}
+
+	override fun onAttachedToWindow() {
+		super.onAttachedToWindow()
+
+		model.items
+				.observeOn(AndroidSchedulers.mainThread())
+				.subscribe { adapter.setItems(it) }
+				.apply { subscriptions.add(this) }
+	}
+
+	override fun onDetachedFromWindow() {
+		subscriptions.unsubscribe()
+		super.onDetachedFromWindow()
+	}
+}
