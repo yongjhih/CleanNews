cd010c7671ef7aa6842492315ff5d01edb338574
Michael Pardo <kensuke155@gmail.com>
2016-03-23 11:25:23 -0400

    Add stub views.

---
+++ android/src/main/kotlin/clean/news/ui/item/detail/ItemDetailView.kt
@@ -0,0 +1,11 @@
+package clean.news.ui.item.detail
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.util.AttributeSet
+
+class ItemDetailView : RecyclerView {
+	@JvmOverloads
+	constructor(context: Context, attrs: AttributeSet? = null, defStyle: Int = 0) : super(context, attrs, defStyle) {
+	}
+}
---
+++ android/src/main/kotlin/clean/news/ui/item/url/UrlView.kt
@@ -0,0 +1,11 @@
+package clean.news.ui.item.url
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.RelativeLayout
+
+class UrlView : RelativeLayout {
+	@JvmOverloads
+	constructor(context: Context, attrs: AttributeSet? = null, defStyle: Int = 0) : super(context, attrs, defStyle) {
+	}
+}
